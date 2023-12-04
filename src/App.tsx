fca6e623bbbbaff12e8e16ff0a6e59e931f25b6d
soojin <soojinchoi323@gmail.com>
2023-12-03 20:00:43 -0800

    setup firebase
    add loading page
    add firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
 const router = createBrowserRouter([
   {
@@ -45,10 +47,20 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    //wait for firebase
+    // setTimeout(() => setIsLoading(false), 2000);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   );
 }
---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALwYq3RetYoqWI22QeS6s54EJNSX8DAE8",
+  authDomain: "fir-twitter-project.firebaseapp.com",
+  projectId: "fir-twitter-project",
+  storageBucket: "fir-twitter-project.appspot.com",
+  messagingSenderId: "906773925237",
+  appId: "1:906773925237:web:8b927631c559bcba48173c",
+};
+
+const app = initializeApp(firebaseConfig);
